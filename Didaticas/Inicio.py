4e460cc81ec7a6e964b17211fa2b6906265098da
Filipe <148493160+Hyaga@users.noreply.github.com>
2025-06-10 21:51:50 -0300

    Mudança de dados

--- Didaticas/Inicio.py
+++ Didaticas/Inicio.py
@@ -1,8 +1,16 @@
 print("Hello Word")
 print("Este sera o inicio do meu aprendizado com o back end em python")
 
+# declaração tipos de dados 
 idade = 23
 numerofloat = 33.33
 nome = "Filipe"
+
+print("Meu nome e :",nome, "meu conhecimento sobre back-end",numerofloat ,"minha idade :",idade)
+
+# alteração de dados sem 
+idade = 22
+numerofloat = 20.22
+nome= "Pedro"
 
 print("Meu nome e :",nome, "meu conhecimento sobre back-end",numerofloat ,"minha idade :",idade)
