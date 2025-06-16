46ff9e11f8cc732f651057891b013c7ef96e3dd1
Filipe <148493160+Hyaga@users.noreply.github.com>
2025-06-15 21:39:08 -0300

    Estruturas condicionais
    Estruturas condicionais usando um exemplo de if else e elif

--- Didaticas/estruturas.py
+++ Didaticas/estruturas.py
@@ -7,4 +7,21 @@ def sacar(valor):
         print("Valor valido")
         print("Retire o dinheiro no caixa")
 
-sacar(600)
+# estruturas condicionais são os if else e ilif
+
+opcao = int(input("Informe qual opcao voce deseja escolher [1] Verificar idade [2] Sacar: "))
+
+
+if opcao == 1:
+    MAIOR_IDADE = 18
+    idade = int(input("Informe sua idade : "))
+    
+    if idade >= MAIOR_IDADE:
+        print("Voce e maior de idade")
+    else:
+        print("Voce e menor de idade")
+        
+elif opcao == 2:
+    sacar(400) 
+else:
+    print("Voce digitou uma opção invalida")
