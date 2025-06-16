c31af9408473d5ab584d2ad6fa43a538e25cd9b8
Filipe <148493160+Hyaga@users.noreply.github.com>
2025-06-15 22:19:50 -0300

    Update do codigo
    Inclui o while para melhorar o codigo

--- Didaticas/estruturas.py
+++ Didaticas/estruturas.py
@@ -4,24 +4,30 @@ def sacar(valor):
     saldo = 500
 
     if saldo >= valor: 
-        print("Valor valido")
+        print("Valor válido")
         print("Retire o dinheiro no caixa")
-
-# estruturas condicionais são os if else e ilif
-
-opcao = int(input("Informe qual opcao voce deseja escolher [1] Verificar idade [2] Sacar: "))
-
-
-if opcao == 1:
-    MAIOR_IDADE = 18
-    idade = int(input("Informe sua idade : "))
-    
-    if idade >= MAIOR_IDADE:
-        print("Voce e maior de idade")
     else:
-        print("Voce e menor de idade")
+        print("Saldo insuficiente")
+
+
+MAIOR_IDADE = 18
+
+# Loop para garantir que o usuário escolha uma opção válida
+while True:
+    opcao = int(input("Informe qual opção você deseja escolher [1] Verificar idade [2] Sacar: "))
+
+    if opcao == 1:
+        idade = int(input("Informe sua idade: "))
         
-elif opcao == 2:
-    sacar(400) 
-else:
-    print("Voce digitou uma opção invalida")
+        if idade >= MAIOR_IDADE:
+            print("Você é maior de idade")
+        else:
+            print("Você é menor de idade")
+        break  # Sai do while após execução
+
+    elif opcao == 2:
+        sacar(400)
+        break  # Sai do while após execução
+
+    else:
+        print("Você digitou uma opção inválida. Tente novamente.")
