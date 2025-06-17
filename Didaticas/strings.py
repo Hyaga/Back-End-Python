bfb355ad6c220597df872b75960cce695b6d145a
Filipe <148493160+Hyaga@users.noreply.github.com>
2025-06-16 21:57:15 -0300

    String
    Aprendizado de string e alguns metodos

---
+++ Didaticas/strings.py
@@ -0,0 +1,22 @@
+# strings 
+
+curso = "pyThon E o flUXo"
+
+print(curso.upper()) # tudo maiusculo
+
+print(curso.lower()) # tudo minusculo
+
+print(curso.title()) # Titulo
+
+print(curso.center(12, "#")) # Junções
+
+print(".".join(curso)) # Junções
+
+
+palavra_exemplo1 = "   Longe   "
+
+print(palavra_exemplo1.strip()) # Tira todos espaços 
+
+print(palavra_exemplo1.lstrip()) # Tira todos espaços esquerda
+
+print(palavra_exemplo1.rstrip()) #  Tira todos espaços direita
