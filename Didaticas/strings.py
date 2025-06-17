abb00582f3c3e17ba4beb3d2588087f0a82e58d1
Filipe <148493160+Hyaga@users.noreply.github.com>
2025-06-16 22:22:46 -0300

    Interpolação de string
    Aqui e como organizamos e chamamos variaveis usando string

--- Didaticas/strings.py
+++ Didaticas/strings.py
@@ -20,3 +20,18 @@ print(palavra_exemplo1.strip()) # Tira todos espaços
 print(palavra_exemplo1.lstrip()) # Tira todos espaços esquerda
 
 print(palavra_exemplo1.rstrip()) #  Tira todos espaços direita
+
+
+nome =  input("Digite seu nome:")
+idade = int(input("Digite sua idade:"))
+materia = input("Digite seu curso :")
+profissão = input("Digite sua profissao:")
+
+# interpolação f - string
+print(f"Olá , me chamo {nome}. Eu tenho{idade} anos, trabalho com {profissão} e estou cursando {materia}")
+
+# edição usando f - string
+
+PI = 3.145545
+print(f"Valor de PI editado : {PI:2f}")
+print(f"Valor de PI editado : {PI:10.2f}")
