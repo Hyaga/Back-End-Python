c72aaef19db962b98121cc9177fb58250f0c61bb
Filipe <148493160+Hyaga@users.noreply.github.com>
2025-06-19 18:56:33 -0300

    Declarção de Listas
    Declaração de listas diferentes em python

---
+++ Didaticas/listas.py
@@ -0,0 +1,10 @@
+#listas 
+
+listas_normais = ["Nome",22, True] # lista normal 
+
+lista_vazia = []
+
+lista_string = list("Python") #Lista aonde cada letra do elemento digitado se torna um item da lista 
+
+print(listas_normais)
+print(lista_string)
