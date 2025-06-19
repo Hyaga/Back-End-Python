7fb96dd77b43ab64235ce6094473866a9256abee
Filipe <148493160+Hyaga@users.noreply.github.com>
2025-06-19 19:28:53 -0300

    Matriz
    Declaração de matriz e como ela e chamada

--- Didaticas/listas.py
+++ Didaticas/listas.py
@@ -8,4 +8,16 @@ lista_string = list("Python") #Lista aonde cada letra do elemento digitado se to
 
 print(listas_normais[1])
 print(lista_string[0])
-print(listas_normais[-1])
+print(listas_normais[-1]) # quanto chamamos numeros negativos estaremos chamando os itens no final da lista
+
+# Matriz
+
+primeira_matriz = [
+    ["Filipe ",20,"Triste"],
+    ["Raiva",24,"Inseguranca"],
+    [2,22,111],
+]
+
+print(primeira_matriz[0])
+print(primeira_matriz[0][0])
+print(primeira_matriz[1][2])
