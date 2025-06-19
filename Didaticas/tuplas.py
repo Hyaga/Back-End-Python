e135114c713c8711bb383b37837d319e7e91be3f
Filipe <148493160+Hyaga@users.noreply.github.com>
2025-06-19 20:28:56 -0300

    Criação de tuplas
    Tupla e muito parecido com as listas a unica diferença notavel e que a tupla não e mutavel ou seja uma vez declarada ela não sera alterada

---
+++ Didaticas/tuplas.py
@@ -0,0 +1,15 @@
+frutas = ("maçã", "laranja", "uva", "pera",)
+
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple([1, 2, 3, 4])
+print(numeros)
+
+pais = ("Brasil",)
+
+print(pais)
+print(frutas[0])  # maçã
+print(frutas[-2])  # uva
