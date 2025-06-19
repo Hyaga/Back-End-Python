e192b79e8e391fdf664a396aefd5462c195595d0
Filipe <148493160+Hyaga@users.noreply.github.com>
2025-06-19 20:43:36 -0300

    Declaração de conjuntos

--- Didaticas/listas.py
+++ Didaticas/listas.py
@@ -58,3 +58,12 @@ for numero in numeros_normais:
 
 print(numeros_pares)
 print(numero_elevados_quadrado)
+
+numeros = set([1, 2, 3, 1, 3, 4])
+print(numeros)  # {1, 2, 3, 4}
+
+letras = set("abacaxi")
+print(letras)  # {"b", "a", "c", "x", "i"}
+
+carros = set(("palio", "gol", "celta", "palio"))
+print(carros)  # {"gol", "celta", "palio"}
