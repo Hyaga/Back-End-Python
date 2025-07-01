43b35858830beb2c3c7881995a1f299fd822b0c1
Filipe <148493160+Hyaga@users.noreply.github.com>
2025-06-30 21:37:37 -0300

    Chamada dos metodos

--- Didaticas/classes.py
+++ Didaticas/classes.py
@@ -9,8 +9,22 @@ class Carro:
         print("Plim plim...")
 
     def parar(self):
-        print("Parando bicicleta...")
-        print("Bicicleta parada!")
+        print("Parando carro...")
+        print("Carro parada!")
 
     def correr(self):
         print("Vrummmmm...")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+
+b1 = Carro("vermelha", "BMW", 2022, 63300)
+b1.buzinar()
+b1.correr()
+b1.parar()
+print(b1.cor, b1.modelo, b1.ano, b1.valor)
+
+b2 = Carro("verde", "BYD", 2000, 22189)
+print(b2)
+b2.correr()
