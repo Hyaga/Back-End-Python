a02c7e0b9f12f1ea04cf8b0c0b923d97bbecfa96
Filipe <148493160+Hyaga@users.noreply.github.com>
2025-06-30 21:36:09 -0300

    Criação de metodos na classe
    Aqui são criados os metodos da classe

--- Didaticas/classes.py
+++ Didaticas/classes.py
@@ -1,6 +1,16 @@
-class Bicicleta:
+class Carro:
     def __init__(self, cor, modelo, ano, valor):
         self.cor = cor
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
+
+    def buzinar(self):
+        print("Plim plim...")
+
+    def parar(self):
+        print("Parando bicicleta...")
+        print("Bicicleta parada!")
+
+    def correr(self):
+        print("Vrummmmm...")
