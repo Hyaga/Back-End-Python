adcde4527870abd8de31dd0afc3657ea5eb800fe
Filipe <148493160+Hyaga@users.noreply.github.com>
2025-07-02 22:18:18 -0300

    Declaraçao da classe abstratada

---
+++ Didaticas/classes_abstratas.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class ControleRemoto(ABC):
+    @abstractmethod
+    def ligar(self):
+        pass
+
+    @abstractmethod
+    def desligar(self):
+        pass
+
+    @property
+    @abstractmethod
+    def marca(self):
+        pass
