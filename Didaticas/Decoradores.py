12ea74f0323a963e03ab110b38a1732d91b6f7b8
Filipe <148493160+Hyaga@users.noreply.github.com>
2025-07-23 21:54:43 -0300

    Condicional de caso
    Uma forma de chamar um condicional de caso em outras linguagens seria o switch e case

--- Didaticas/Decoradores.py
+++ Didaticas/Decoradores.py
@@ -12,3 +12,36 @@ def executar(funcao, nome): # Aqui que ocorre o diferencial pois se não tivesse
 
 print(executar(mensagem, "Joao"))
 print(executar(mensagem_longa, "Joao"))
+
+def calculadora(operacao):
+    def soma(a, b):
+        return a + b
+
+    def sub(a, b):
+        return a - b
+
+    def mul(a, b):
+        return a * b
+
+    def div(a, b):
+        return a / b
+
+    match operacao:
+        case "+":
+            return soma
+        case "-":
+            return sub
+        case "*":
+            return mul
+        case "/":
+            return div
+
+
+op = calculadora("+")
+print(op(2, 2))
+op = calculadora("-")
+print(op(2, 2))
+op = calculadora("*")
+print(op(2, 2))
+op = calculadora("/")
+print(op(2, 2))
